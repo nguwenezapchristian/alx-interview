40c0b0db3cf8548e2183dc35b46b6924656cc9c5
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-04-23 21:30:32 +0200

    using list comprehension

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,23 +8,16 @@ representing the Pascal's triangle of n(rows)
 
 def pascal_triangle(n):
     triangle_list = []
+    """ returns an empty list if n <= 0 """
     if n <= 0:
         return triangle_list
     i = 0
     while i < n:
-        row = []
-        j = 0
-        while j <= i:
-            if i == 0 or j == 0:
-                num = 1
-            else:
-                """ Here i used // to get integer instead of a float
-                    and this formula num = (num * (i - j + 1)) // j is
-                    used to generate the exact value on each row and column
-                """
-                num = (num * (i - j + 1)) // j
-            row.append(num)
+        row = [1] * (i + 1)
+        j = 1
+        while j < i:
+            row[j] = triangle_list[i - 1][j - 1] + triangle_list[i - 1][j]
             j += 1
         triangle_list.append(row)
         i += 1
-    return triangle_list
+    return (triangle_list)
