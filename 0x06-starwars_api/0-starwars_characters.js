d209a3ef1d6f609621c17fa5248a9e434ac3c624
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-11 16:51:13 +0200

    followed semistandard rules

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,39 +4,39 @@
 
 const request = require('request');
 
-function getCharactersArray() {
-    const movieId = process.argv[2];
-    const apiLink = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+function getCharactersArray () {
+  const movieId = process.argv[2];
+  const apiLink = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-    request(apiLink, { json: true }, (error, response, body) => {
-        if (error) {
-            return console.error('Error:', error);
-        }
-        if (response.statusCode !== 200) {
-            return console.error('Failed to retrieve movie:', response.statusCode);
-        }
-        const characterArray = body.characters;
-        printCharacterNames(characterArray, 0);
-    });
+  request(apiLink, { json: true }, (error, response, body) => {
+    if (error) {
+      return console.error('Error:', error);
+    }
+    if (response.statusCode !== 200) {
+      return console.error('Failed to retrieve movie:', response.statusCode);
+    }
+    const characterArray = body.characters;
+    printCharacterNames(characterArray, 0);
+  });
 }
 
 // Fetch and print all movie character names recursively
-function printCharacterNames(characterArray, index) {
-    if (index >= characterArray.length) {
-        return;
-    }
+function printCharacterNames (characterArray, index) {
+  if (index >= characterArray.length) {
+    return;
+  }
 
-    const characterLink = characterArray[index];
-    request(characterLink, { json: true }, (error, response, body) => {
-        if (error) {
-            return console.error('Error:', error);
-        }
-        if (response.statusCode !== 200) {
-            return console.error('Failed to retrieve character:', response.statusCode);
-        }
-        console.log(body.name);
-        printCharacterNames(characterArray, index + 1);
-    });
+  const characterLink = characterArray[index];
+  request(characterLink, { json: true }, (error, response, body) => {
+    if (error) {
+      return console.error('Error:', error);
+    }
+    if (response.statusCode !== 200) {
+      return console.error('Failed to retrieve character:', response.statusCode);
+    }
+    console.log(body.name);
+    printCharacterNames(characterArray, index + 1);
+  });
 }
 
 getCharactersArray();
