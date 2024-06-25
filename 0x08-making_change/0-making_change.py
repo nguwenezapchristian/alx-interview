18e81c3b92334eaa0174ff4bd06c0b99cce0634b
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-25 19:48:34 +0200

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    """Create a list to store the minimum number
+    of coins needed for each amount up to the total.
+    Return the minimum number of coins needed for each
+    amount up to the total, or -1 if it is not possible
+    to make change for each amount up to the total
+    """
+    dp = [float('inf')] * (total + 1)
+    
+    # Base case: 0 coins are needed to make 0 total
+    dp[0] = 0
+    
+    # Iterate over each coin value
+    for coin in coins:
+        # Iterate over each amount up to the total
+        for amount in range(coin, total + 1):
+            # Update the minimum number of coins needed for the current amount
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    
+    # If the minimum number of coins needed for the total is still infinity, return -1
+    if dp[total] == float('inf'):
+        return -1
+    # Otherwise, return the minimum number of coins needed
+    return dp[total]
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,21 @@
+# Making Change
+
+This project tackles the coin change problem, which involves finding the minimum number of coins required to make up a given total amount, given a list of coin denominations.
+
+## Installation
+
+To run this project, you can clone the repository and navigate to the `0x08-making_change` directory.
+
+```bash
+git clone https://github.com/alx-interview.git
+cd alx-interview/0x08-making_change
+./0-main.py
+```
+```
+print(makeChange([1, 2, 25], 37))
+print(makeChange([1256, 54, 48, 16, 102], 1453))
+```
+```
+7
+-1
+```
