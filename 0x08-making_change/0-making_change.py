61adefeb26c3b6ef680c432d8116f361030cb17a
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-25 19:51:11 +0200

    formatting document

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,19 +7,23 @@ def makeChange(coins, total):
     to make change for each amount up to the total
     """
     dp = [float('inf')] * (total + 1)
-    
-    # Base case: 0 coins are needed to make 0 total
+
+    """Base case: 0 coins are needed to make 0 total"""
     dp[0] = 0
-    
-    # Iterate over each coin value
+
+    """Iterate over each coin value"""
     for coin in coins:
-        # Iterate over each amount up to the total
+        """Iterate over each amount up to the total"""
         for amount in range(coin, total + 1):
-            # Update the minimum number of coins needed for the current amount
+            """Update the minimum number of coins needed
+            for the current amount
+            """
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
-    # If the minimum number of coins needed for the total is still infinity, return -1
+
+    """If the minimum number of coins needed for the total is still infinity,
+    return -1
+    """
     if dp[total] == float('inf'):
         return -1
-    # Otherwise, return the minimum number of coins needed
+    """Otherwise, return the minimum number of coins needed for the total"""
     return dp[total]
